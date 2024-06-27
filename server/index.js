bb5ce41a0600f43dfd1aeab813a1c3f269edf873
udits6174 <udits502@gmail.com>
2024-06-27 08:41:01 +0530

    Add scripts in BE

--- server/index.js
+++ server/index.js
@@ -16,32 +16,17 @@ app.get("/*", async(req, res) => {
   const headers = {
     Authorization: `Bearer ${ACCESS_TOKEN}`,
   };
-  const path = req.params[0];
+  const path  = req.params[0];
   const params = req.query;
   try {
-    const response = await axios.get(TMDB_BASE_URL + path, {
-      headers,
-      params,
-    });
-    res.json(response.data);
-    console.log(response.data);
+      const response = await axios.get(TMDB_BASE_URL + path, {
+        headers,
+        params,
+      });
+      res.json(response.data);
+      console.log(response.data);
   } catch (error) {
-    if (error.response) {
-      // The request was made and the server responded with a status code
-      // that falls out of the range of 2xx
-      console.error(error.response.data);
-      console.error(error.response.status);
-      console.error(error.response.headers);
-      res.status(error.response.status).json(error.response.data);
-    } else if (error.request) {
-      // The request was made but no response was received
-      console.error(error.request);
-      res.status(500).json({ error: 'No response received from TMDB' });
-    } else {
-      // Something happened in setting up the request that triggered an Error
-      console.error('Error', error.message);
-      res.status(500).json({ error: error.message });
-    }
+    res.json(error);
   }
 });
 
